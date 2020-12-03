f0307b3e7a508a022b04b08f32eca8a11c5a853e
jshww <jshwee@gmail.com>
2020-12-03 16:43:51 +0800

    added comments

--- trainer.py
+++ trainer.py
@@ -24,8 +24,11 @@ from utils import *
 
 email_df = pd.read_pickle("./data.pkl")
 
-#Training
+#=========== Training ==============
 Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(email_df['clean_text'], email_df['label'], test_size=0.3)
+
+tfidf_vect = TfidfVectorizer(analyzer='word', stop_words='english', max_features=4000)
+tfidf_vect.fit(email_df['clean_text'])
 
 Train_X = tfidf_vect.transform(Train_X)
 Test_X = tfidf_vect.transform(Test_X)
@@ -35,26 +38,31 @@ SVM.fit(Train_X, Train_Y)
 
 predictions_SVM = SVM.predict(Test_X)
 print("SVM Accuracy Score -> ", accuracy_score(predictions_SVM, Test_Y)*100)
+#=========== Training ==============
 
-#======== Saving Model ===========
-accuracy = accuracy_score(predictions_SVM, Test_Y)*100
+#=========== Saving model ===========
+
+accuracy = "SVM Accuracy Score -> ", accuracy_score(predictions_SVM, Test_Y)*100
 report = classification_report(Test_Y, predictions_SVM)
-cfm = cf_64(SVM, Test_X, Test_Y)
+cfm = cf_64(SVM, Test_X, Test_Y))
 
 filename = "svm_model.pkl"
-pickle.dump({'model': SVM, 'accuracy': accuracy, 'report': report, 'confusion': cfm}, open(filename, 'wb'))
-#=================================
+pickle.dump({'model': SVM, 'report': report, 'cfm': cfm, 'accuracy': accuracy}, open(filename, 'wb'))
 
-new_text = convert_text(x, tfidf_vect)
+#=========== Saving model ===========
 
-prediction = SVM.predict(new_text)
-prediction_prob = SVM.predict_proba(new_text)
-prob = [max(x) for x in prediction_prob]
-prob_min = [min(x) for x in prediction_prob]
 
-print(prob_min)
-print(prob)
-print(prediction)
+#
+# new_text = convert_text(x, tfidf_vect)
+#
+# prediction = SVM.predict(new_text)
+# prediction_prob = SVM.predict_proba(new_text)
+# prob = [max(x) for x in prediction_prob]
+# prob_min = [min(x) for x in prediction_prob]
+#
+# print(prob_min)
+# print(prob)
+# print(prediction)
 
 
 
