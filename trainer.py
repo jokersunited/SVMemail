f859814cb8e034fdbd30ac10a0e09ef0badc6a99
jshww <jshwee@gmail.com>
2020-12-03 14:15:16 +0800

    Tried graphs

--- trainer.py
+++ trainer.py
@@ -7,37 +7,20 @@ from nltk import pos_tag
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
+from sklearn import svm
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn import model_selection, svm
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report, plot_confusion_matrix
 import pickle
 
 from utils import *
 
-# truncate = 2450
-# length = 100
-#
-# email_df = pd.read_csv("spam_or_not_spam.csv", dtype=str)[truncate:truncate+length]
-# email_df['email'].dropna(inplace=True)
-#
-# email_df['email'] = [word_tokenize(str(row).lower()) for row in email_df['email']]
-#
-# tag_map = word_tag()
-#
-# for index, text in enumerate(email_df['email']):
-#     print(index)
-#     clean_words = []
-#
-#     word_lem = WordNetLemmatizer()
-#     for word, tag in pos_tag(text):
-#         if word not in stopwords.words('english') and word.isalpha():
-#             clean_word = word_lem.lemmatize(word, tag_map[tag[0]])
-#             clean_words.append(str(clean_word))
-#     print(clean_words)
-#     email_df.loc[index+truncate, 'clean_text'] = str(clean_words)
-#
-#
-# email_df.to_pickle("./data.pkl")
+# load_dataset("spam_or_not_spam.csv", "data.pkl")
 
 email_df = pd.read_pickle("./data.pkl")
 
@@ -46,50 +29,69 @@ Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(email_df['cl
 
 tfidf_vect = TfidfVectorizer(max_features=4000)
 tfidf_vect.fit(email_df['clean_text'])
+# vectorizer = CountVectorizer(email_df['clean_text'])
 
-print(Test_X)
+x = ["Your parcel is ready to be collected, please click this link", "your mother father is fine too", "what the hell you doing sir", "you are a cunt bag"]
 
 Train_X = tfidf_vect.transform(Train_X)
 Test_X = tfidf_vect.transform(Test_X)
 
-SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+# plt.spy(Train_X)
+# plt.show()
+# exit()
+#
+# # for item in Train_X:
+# #     print(item.
+# #     exit()
+# # print(Train_X[0])
+# # print(Test_X.toarray())
+
+tfidf_df = pd.DataFrame(Test_X.todense())
+for index, item in tfidf_df.iterrows():
+    print(type(item))
+    print(item.index)
+
+
+
+print(tfidf_df)
+# tfidf_df = np.expand_dims(tfidf_df.to_numpy(), 1)
+# print(tfidf_df)
+
+print(len(tfidf_df.columns))
+print(len(tfidf_df.values[1,:]))
+print(len(tfidf_df.values))
+plt.scatter(tfidf_df.columns, tfidf_df.values[1,:], c=Test_Y)
+plt.show()
+
+
+
+print(tfidf_df)
+print(Train_X)
+print(type(Train_X))
+
+
+SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto', probability=True)
 SVM.fit(Train_X, Train_Y)
 
 predictions_SVM = SVM.predict(Test_X)
+print(classification_report(Test_Y, predictions_SVM))
+print(cf_64(SVM, Test_X, Test_Y))
 
 filename = "svm_model.pkl"
 pickle.dump(SVM, open(filename, 'wb'))
 
-print(predictions_SVM)
 print("SVM Accuracy Score -> ", accuracy_score(predictions_SVM, Test_Y)*100)
 
-test = input("\n\nEnter email text here to check")
+new_text = convert_text(x, tfidf_vect)
 
-word_list = word_tokenize(str(test).lower())
-print(word_list)
-clean_words = []
-tag_map = word_tag()
-word_lem = WordNetLemmatizer()
-for word, tag in pos_tag(word_list):
-    if word not in stopwords.words('english') and word.isalpha():
-        clean_word = word_lem.lemmatize(word, tag_map[tag[0]])
-        clean_words.append(str(clean_word))
+prediction = SVM.predict(new_text)
+prediction_prob = SVM.predict_proba(new_text)
+prob = [max(x) for x in prediction_prob]
+prob_min = [min(x) for x in prediction_prob]
 
-test_df = pd.DataFrame(columns=["clean_words"], dtype=object)
-test_df.loc[0, 'clean_words'] = str(clean_words)
-
-print(test_df)
-
-test_tfidf = tfidf_vect.transform(test_df['clean_words'])
-
-print(test_tfidf)
-
-prediction = SVM.predict(test_tfidf)
+print(prob_min)
+print(prob)
 print(prediction)
-if prediction:
-    print("PHISHING!!!")
-else:
-    print("CLEAN!!!")
 
 
 
